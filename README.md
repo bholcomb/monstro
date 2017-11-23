3c71e37ef988b3e84eae98d2209e8e2fd7ade5bd
Bob Holcomb <bob_holcomb@hotmail.com>
2017-11-23 13:20:51 -0500

    Initial commit of build system, 3rdparty lib, and placeholder files.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# monstro
-C++ game UI library and editor.  
+# Monstro
+C++ game UI library and editor.  Looking to try some experiments with something that is IMGUI like, but still themable and easy to use as an in-game UI.  Also looking to have an editor to help put together a UI.
+
+Monstro is latin for "I show" (allegdly, Google said so, so it must be true)
---
+++ build/premake5.lua
@@ -0,0 +1,34 @@
+solution "Monstro"
+   location("../")
+   configurations { "Debug", "Release" }
+   platforms{"x64"}
+   startproject "testUi"
+
+   includedirs("../include", "../3rdParty/include")
+   libdirs("../lib", "../3rdParty/lib")
+
+   
+  configuration { "Debug" }
+    defines { "DEBUG", "TRACE"}
+    symbols "On"
+    optimize "Off"
+ 
+  configuration { "Release" }
+    optimize "Speed"
+	
+   
+project "ui"
+	kind "SharedLib"
+	language "C++"
+	location "ui"
+	targetdir("../lib")
+	files{"../include/ui/*.h", "../libsrc/ui/*.cxx"}
+	vpaths { ["Headers"] = "../include/ui/*.h", ["Source"] = "../libsrc/ui/*.cxx" }
+	
+project "testUi"
+	kind "ConsoleApp"
+	language "C++"
+	location "testUi"
+	targetdir("../bin")
+	files{"libsrc/testUi/*.cxx"}
+	links {"ui", "glfw", "OpenGL32"}
